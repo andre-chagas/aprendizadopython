d692f99f40ad51df5325dddeccf1c6cd43fd6e2a
Andre Chagas <andreluis_chagas@hotmail.com>
2023-11-09 23:32:55 -0300

    acrescentado aulas de classes e métodos

---
+++ aula160.py
@@ -0,0 +1,24 @@
+# Class - Classes são moldes para criar novos objetos
+# As classes geram novos objetos (instâncias) que podem ter seus próprios atributos e métodos
+# Os objetos gerados pela classe poder usar dados internos para realizar várias ações
+# Por convenção, usamos PascalCase para nomes de classes
+
+class Pessoa:
+     def __init__(self, nome, sobrenome):
+          self.nome = nome
+          self.sobrenome = sobrenome 
+          
+
+p1 = Pessoa('André', 'Chagas')
+# p1.nome = 'André'
+# p1.sobrenome = 'Chagas'
+
+p2 = Pessoa('Maria', 'Joana')
+# p2.nome = 'Maria'
+# p2.sobrenome = 'Joana'
+
+print(p1.nome)
+print(p1.sobrenome)
+
+print(p2.nome)
+print(p2.sobrenome)
