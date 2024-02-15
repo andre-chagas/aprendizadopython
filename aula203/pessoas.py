2d90ee8bf7804e539f2cdef75e6343f950518d8e
Andre Chagas <andreluis_chagas@hotmail.com>
2024-02-14 23:57:33 -0300

    dataclasses

--- aula203/pessoas.py
+++ aula203/pessoas.py
@@ -22,11 +22,6 @@ class Pessoa:
     def idade(self, idade: int):
         self._idade = idade
 
-    def __repr__(self):
-        class_name = type(self).__name__
-        attrs = f'({self.nome!r}, {self.idade!r})'
-        return f'{class_name}{attrs}'
-
 
 class Cliente(Pessoa):
     def __init__(self, nome: str, idade: int) -> None:
