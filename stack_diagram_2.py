28c128323884395a4629aa72dfb06670b9978890
Andre Chagas <andreluis_chagas@hotmail.com>
2024-07-05 22:32:10 -0300

    Deque - trabalhando com LIFO e FIFO

---
+++ stack_diagram_2.py
@@ -0,0 +1,27 @@
+"""
+Pilha (Stack)
+Pilha é uma estrutura de dados abstrata que segue o princípio LIFO (Last In
+First Out) - último item a entrar, será o primeiro a sair.
+A adição e remoção de novos itens ocorre sempre na mesma ponta. O final da
+pilha, representa seu topo, o começo da pilha representa sua base.
+
+Podemos usar listas para representar pilhas em Python já que só precisamos dos
+métodos append, para adicionar itens ao topo da pilha e pop, para remover
+itens do topo da pilha.
+
+Exemplos abaixo são apenas para append
+"""
+from typing import List
+
+stack: List[str] = []
+
+stack.append('A')
+stack.append('B')
+stack.append('C')
+
+top_item = stack.pop() # C
+top_item = stack.pop() # B
+top_item = stack.pop() # A
+
+
+print(stack, top_item)
