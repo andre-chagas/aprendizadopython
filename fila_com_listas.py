954fb6512fca82ba223bafa2fcf0d222ba1481e2
Andre Chagas <andreluis_chagas@hotmail.com>
2024-07-16 20:26:51 -0300

    filas em python - estruturas de dados - Queue

---
+++ fila_com_listas.py
@@ -0,0 +1,16 @@
+# FIFO - First in First Out
+#   0    1    2
+# ['A', 'B', 'C']
+queue = []
+queue.append('A')
+print(queue)
+queue.append('B')
+print(queue)
+queue.append('C')
+print(queue)
+print(queue.pop(0))
+print(queue)
+print(queue.pop(0))
+print(queue)
+print(queue.pop(0))
+print(queue)
